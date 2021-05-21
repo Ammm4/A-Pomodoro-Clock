e3745c623bcf63e57f7aa2c961c55c90a7bbd056
Ammm4 <ammm.4amit@gmail.com>
2021-05-21 12:48:43 +0100

    Updates

---
+++ precache-manifest.60c381370cc26fbb0771d66b3af57a98.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e1b0cf303a84647b7aefb1094153430",
+    "url": "/A-Pomodoro-Clock/index.html"
+  },
+  {
+    "revision": "11edba14d39cc0e3d8fa",
+    "url": "/A-Pomodoro-Clock/static/css/main.5beb02cd.chunk.css"
+  },
+  {
+    "revision": "8c367c791e99abd9d69a",
+    "url": "/A-Pomodoro-Clock/static/js/2.cafa7aa9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/A-Pomodoro-Clock/static/js/2.cafa7aa9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11edba14d39cc0e3d8fa",
+    "url": "/A-Pomodoro-Clock/static/js/main.c65d574a.chunk.js"
+  },
+  {
+    "revision": "d3c60f47e5b2af46895e",
+    "url": "/A-Pomodoro-Clock/static/js/runtime-main.2c485b3d.js"
+  }
+]);
